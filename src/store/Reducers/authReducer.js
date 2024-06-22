9d03d00cc94a91fe7b8395075146580bb7b07adf
Johnny Wu <skydodle@gmail.com>
2024-06-22 15:38:35 -0700

    feat: add authReducer

---
+++ src/store/Reducers/authReducer.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authReducer = createSlice({
+  name: 'auth',
+  initialState: {
+    successMessage: '',
+    errorMessage: '',
+    loader: false,
+    userInfo: '',
+  },
+  reducers: {},
+  extraReducers: () => {},
+});
+
+export default authReducer.reducer;
