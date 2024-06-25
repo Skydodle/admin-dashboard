d3a77a574f5dde00698295a8dca5ac5735e6b71e
Johnny Wu <skydodle@gmail.com>
2024-06-25 11:12:17 -0700

    feat: add react toaster for login notification

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 /** @format */
 
-import React, { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import { Toaster } from 'react-hot-toast';
 
 const App = lazy(() => import('./App.jsx'));
 
@@ -14,6 +15,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <Suspense>
         <App />
+        <Toaster
+          toastOptions={{
+            position: 'top-right',
+            style: {
+              background: '#283046',
+              color: 'white',
+            },
+          }}
+        />
       </Suspense>
     </Provider>
   </BrowserRouter>
